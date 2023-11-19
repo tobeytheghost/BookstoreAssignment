a8060a606b724fe240a19efbe82e21b991a4b2ac
falzo <falzo@TobeyTheGhost>
2023-11-18 20:23:18 -0500

    Working Registration and Login

--- BookstoreAssignment_MariahFalzon/src/main/resources/data.sql
+++ BookstoreAssignment_MariahFalzon/src/main/resources/data.sql
@@ -5,11 +5,11 @@ INSERT INTO books (isbn, title, author, price, description) VALUES
 (8503, 'The Hunger Games', 'Suzanne Collins', 14.99, 'DESCRIPTION');
 
 --Populating Information for Admin
-INSERT INTO sec_user (userName, encryptedPassword, enabled)
-VALUES ('admin', '$2a$12$l4zsT9wUr0qqyM1pGO6lK.abgRYMu9NwD5hYj64s.FYVc7KdvL6WS', 1);
+INSERT INTO sec_user (email, userName, encryptedPassword, enabled)
+VALUES ('falzonm@sheridancollege.ca', 'admin', '$2a$12$l4zsT9wUr0qqyM1pGO6lK.abgRYMu9NwD5hYj64s.FYVc7KdvL6WS', 1);
 
-INSERT INTO sec_user (userName, encryptedPassword, enabled)
-VALUES ('test', '$2a$12$l4zsT9wUr0qqyM1pGO6lK.abgRYMu9NwD5hYj64s.FYVc7KdvL6WS', 1);
+INSERT INTO sec_user (email, userName, encryptedPassword, enabled)
+VALUES ('test@test.com', 'test', '$2a$12$l4zsT9wUr0qqyM1pGO6lK.abgRYMu9NwD5hYj64s.FYVc7KdvL6WS', 1);
 
 INSERT INTO sec_role (roleName)
 VALUES ('ROLE_USER');
